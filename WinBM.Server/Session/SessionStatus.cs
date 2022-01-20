84170222b601a5c67c342baa0aa87ee495f7ed31
tgiqfe <tqji31001@gmail.com>
2022-01-20 22:48:47 +0900

    start coding...

---
+++ WinBM.Server/Session/SessionStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinBM.Server.Session
+{
+    internal enum SessionStatus
+    {
+        None = 0,       //  セッション開始前
+        Begin = 1,      //  BeginReceiveProcess / BeginSendProcess
+        Main = 2,       //  MainReceiveProcess / MainSendProcess
+        End = 3,        //  EndReceiveProcess / EndSendProcess
+        Finished = 4,   //  セッション終了済み
+    }
+}
